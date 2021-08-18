0c82ce5d724a21d98119d1e4c057d41a032b6db6
Safeer Ahmed <safeer.ahmed@muchconsulting.de>
2021-08-18 14:18:38 +0500

    initial commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hospital Management',
+    'version' : '1.0',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'https://www.google.com',
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
