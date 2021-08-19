2869b187293ada9cd2439abbf45797b022edd92f
Safeer Ahmed <safeer.ahmed@muchconsulting.de>
2021-08-19 19:59:53 +0500

    refactor[om_hospital]: change formatting

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Hospital Management',
-    'version' : '1.0',
-    'summary': 'Hospital Management Software',
-    'sequence': -100,
-    'description': """Hospital Management Software""",
-    'category': 'Productivity',
-    'website': 'https://www.google.com',
-    'depends' : [],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/patient.xml'
-    ],
-    'demo': [],
-    'qweb': [],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    "name": "Hospital Management",
+    "version": "1.0",
+    "summary": "Hospital Management Software",
+    "sequence": -100,
+    "description": """Hospital Management Software""",
+    "category": "Productivity",
+    "website": "https://www.google.com",
+    "depends": [],
+    "data": ["security/ir.model.access.csv", "views/patient.xml"],
+    "demo": [],
+    "qweb": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -5,12 +5,11 @@ class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
-    gender= fields.Selection([
-        ('male','Male'),
-        ('female','Female'),
-        ('other','Other')
-    ], required=True, default='male')
-    note = fields.Text(string='Description')
-
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age")
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female"), ("other", "Other")],
+        required=True,
+        default="male",
+    )
+    note = fields.Text(string="Description")
